c8ba840a6d2d4704a715bc4af41e18399a0519b6
Alfrid-n <98148020+Alfrid-n@users.noreply.github.com>
2023-06-13 16:15:44 +0800

    update

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,19 +1,43 @@
+// const sha256 = require('crypto-js/sha256')
+import sha256 from 'crypto-js/sha256.js'
+import UTXOPool from './UTXOPool.js'
+
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
-   -名字
-   -创世区块
-   -存储区块的映射
+
   */
-  constructor(blockchain, parentHash, height, hash) {
+  constructor(blockchain, parentHash, height, hash, miner) {
     this.blockchain = blockchain
     this.parentHash = parentHash
     this.height = height
     this.hash = hash
+    this.coinbaseBeneficiary = miner
+    this.utxoPool = new UTXOPool({})
   }
+
   getPreviousBlock () {
+    // 判断是否为高度为1的区块
+    if (this.height == 1) {
+      return this.blockchain.genesis
+    }
     return this.blockchain.blocks[this.parentHash]
   }
+
+  isValid () {
+    const leadingZero = '0'.repeat(DIFFICULTY)
+    this.setHash()
+    return this.hash.startsWith(leadingZero)
+  }
+
+  setNonce (nonce) {
+    this.nonce = nonce
+  }
+  setHash () {
+    this, this.hash = sha256(this.nonce + this.parentHash + this.height + this.blockchain).toString()
+  }
+
 }
 
 export default Block
+export const DIFFICULTY = 3
